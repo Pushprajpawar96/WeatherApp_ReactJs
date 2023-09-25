11f0eb63a968728dd67429ec7198ea5d44a949df
unknown <pawarraj@gmail.com>
2023-09-25 16:30:46 +0530

    Weather_app

---
+++ build/precache-manifest.fb5164dd11c8a6dd50a2d6ee590e2857.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a2c89766c64ab22ac4336ccfcb08f80",
+    "url": "/Weather_APP_ReactJS/index.html"
+  },
+  {
+    "revision": "25cf6a54de7eede5f44a",
+    "url": "/Weather_APP_ReactJS/static/css/main.cc6b319e.chunk.css"
+  },
+  {
+    "revision": "001b67d05a335e326bff",
+    "url": "/Weather_APP_ReactJS/static/js/2.6a7fe4e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather_APP_ReactJS/static/js/2.6a7fe4e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25cf6a54de7eede5f44a",
+    "url": "/Weather_APP_ReactJS/static/js/main.88c97f71.chunk.js"
+  },
+  {
+    "revision": "d491244e340a1029d29b",
+    "url": "/Weather_APP_ReactJS/static/js/runtime-main.5d097ffa.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/Weather_APP_ReactJS/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "31594c3bbae4069ae1e777693fd73ce0",
+    "url": "/Weather_APP_ReactJS/static/media/sunset-2582656_1280.31594c3b.jpg"
+  },
+  {
+    "revision": "a9677d7579eb82ea6903c9c5c0d45690",
+    "url": "/Weather_APP_ReactJS/static/media/thunderstorm-3440450_1280.a9677d75.jpg"
+  }
+]);
