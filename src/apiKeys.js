6d6077e43561047ca2ccb1e55536a5135fb98843
unknown <pawarraj@gmail.com>
2023-09-25 13:31:27 +0530

    weather App

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "b327fb8c145bf0698145c6e8e6c4b14f",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
